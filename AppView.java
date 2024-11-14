d2bb1ab5cecf927aea9d4fd127cdc7f76df03c07
Mirror Wanderer <67412789+TrishaAndalene@users.noreply.github.com>
2024-11-14 17:01:04 +1100

    only adding the checkbox

--- AppView.java
+++ AppView.java
@@ -1,9 +1,6 @@
 // ------------------------- JAVAFX LIBRARY ---------------------------
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -11,16 +8,25 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.layout.HBox;
 
+// ---------------------------- JAVA LIBRARY ----------------------------
+import java.util.*;
+
 public class AppView {
     
     final static int ScreenWidth = 500;
     final static int ScreenHeight = 450;
 
-    public AppView(){
+    public HashMap<String, Scene> scenes;
 
+    public AppView(){
+        this.scenes = new HashMap<>();
+
+        // trigger to for scenes
+        this.createRegisScreen();
     }
 
-    public static String showTitleTemplate(){
+    // only static for now
+    Label showTitleTemplate(){
         String s = "";
 
         // format writing
@@ -29,120 +35,126 @@ public class AppView {
         s += "\n|    |  \\ \\__/ \\__/ |___ \\__,  |     /~~\\";
         s += "\n \n \n";
 
-        return s;
+        Label titleLabel = new Label(s);
+        titleLabel.setFont(Font.font("MonoSpace", FontWeight.EXTRA_BOLD,16));
+        return titleLabel;
     }
 
-    public Scene getRegisScene(){
-         // object
-         String title = AppView.showTitleTemplate();
-         Label titleLabel = new Label(title);
-         titleLabel.setFont(Font.font("MonoSpace", FontWeight.EXTRA_BOLD,16));
-        //  titleLabel.setTextFill(Color.WHITE);
- 
-         // label
-         Label username = new Label("Username: ");
-        //  username.setTranslateY(10);
- 
-         TextField regisAcc = new TextField();
-         regisAcc.setPromptText("Enter new name");
-         regisAcc.setPrefWidth(200);;
- 
-         Label acceptName = new Label("✔   ");
-         acceptName.setTextFill(Color.FORESTGREEN);
-        
-         Label age = new Label("Age:          ");
-        //  age.setTranslateY(35);
- 
-         TextField regisAge = new TextField();
-         regisAge.setPromptText("Enter your age");
-         regisAge.setPrefWidth(200);
- 
-         Label acceptAge = new Label("✔   ");
-         acceptAge.setTextFill(Color.FORESTGREEN);
- 
-         Label pass = new Label("Password: ");
-        //  pass.setTranslateY(50);
- 
-         TextField regisPass = new TextField();
-         regisPass.setPromptText("Enter your password");
-         regisPass.setPrefWidth(200);
- 
-         Label acceptPass = new Label("✔   ");
-         acceptPass.setTextFill(Color.FORESTGREEN);
- 
-        Label accountType = new Label("Account type:   ");
+    // create new windows
 
+    // create all pages/scenes
+    void createRegisScreen(){
+        // object
+        Label titleLabel = this.showTitleTemplate();
+  
+        // label
+        Label username = new Label("Username: ");
+  
+        TextField regisAcc = new TextField();
+        regisAcc.setPromptText("Enter new name");
+        regisAcc.setPrefWidth(200);;
+  
+        Label acceptName = new Label("✔   ");
+        acceptName.setTextFill(Color.FORESTGREEN);
+         
+        Label age = new Label("Age:          ");
+  
+        TextField regisAge = new TextField();
+        regisAge.setPromptText("Enter your age");
+        regisAge.setPrefWidth(200);
+  
+        Label acceptAge = new Label("✔   ");
+        acceptAge.setTextFill(Color.FORESTGREEN);
+  
+        Label pass = new Label("Password: ");
+  
+        TextField regisPass = new TextField();
+        regisPass.setPromptText("Enter your password");
+        regisPass.setPrefWidth(200);
+  
+        Label acceptPass = new Label("✔   ");
+        acceptPass.setTextFill(Color.FORESTGREEN);
+  
+        Label accountType = new Label("Account type:   ");
+ 
         ToggleGroup toggleAccountCreateGroup = new ToggleGroup();
         RadioButton sellerBtn = new RadioButton("Seller");
         sellerBtn.setToggleGroup(toggleAccountCreateGroup);
-
+ 
         RadioButton guestBtn = new RadioButton("Guest");
         guestBtn.setToggleGroup(toggleAccountCreateGroup);
 
-
-         Button createBtn = new Button("Sign up");
-         createBtn.setStyle("-fx-background-color: #7AB2D3");
-         createBtn.setTextFill(Color.WHITE);
-         createBtn.setOnMouseEntered(e -> {
-             createBtn.setStyle("-fx-background-color: white"); 
-             createBtn.setTextFill(Color.BLACK);
-         });
-         createBtn.setOnMouseExited(e -> {
-             createBtn.setStyle("-fx-background-color: #7AB2D3"); 
-             createBtn.setTextFill(Color.WHITE);
-         });
+        CheckBox ageVerify = new CheckBox("Yes, I agree to the term and already 18 by the time       ");
+        Button createBtn = new Button("Sign up");
+        createBtn.setStyle("-fx-background-color: #7AB2D3");
+        createBtn.setTextFill(Color.WHITE);
+        createBtn.setOnMouseEntered(e -> {
+            createBtn.setStyle("-fx-background-color: white"); 
+            createBtn.setTextFill(Color.BLACK);
+        });
+        createBtn.setOnMouseExited(e -> {
+            createBtn.setStyle("-fx-background-color: #7AB2D3"); 
+            createBtn.setTextFill(Color.WHITE);
+        });
+  
+        // fill in first V box
  
-         // fill in first V box
-
         int paddingLeft = 17;
- 
-         HBox horiRootButton = new HBox();
-         horiRootButton.getChildren().addAll(createBtn);
-         horiRootButton.setAlignment(Pos.CENTER);
-        //  horiRootButton.setSpacing(40);
+  
+        HBox horiRootButton = new HBox();
+        horiRootButton.getChildren().addAll(createBtn);
+        horiRootButton.setAlignment(Pos.CENTER);
         horiRootButton.setTranslateX(90);
-        //  horiRootButton.setTranslateY(90);
-
+ 
         HBox horiRootAccount = new HBox();
         horiRootAccount.getChildren().addAll(accountType, guestBtn, sellerBtn);
         horiRootAccount.setAlignment(Pos.CENTER_LEFT);
         horiRootAccount.setSpacing(paddingLeft+25);
- 
-         HBox horiRootage = new HBox(paddingLeft);
-         horiRootage.getChildren().addAll(age, regisAge, acceptAge);
-         horiRootage.setAlignment(Pos.CENTER);
-        //  horiRootage.setTranslateX(60);
-        //  horiRootage.setTranslateY(40);
- 
-         HBox horiRootPass = new HBox(paddingLeft);
-         horiRootPass.getChildren().addAll(pass, regisPass, acceptPass);
-         horiRootPass.setAlignment(Pos.CENTER);
-        //  horiRootPass.setTranslateX(60);
-        //  horiRootPass.setTranslateY(55);
- 
-         HBox horiRoot = new HBox(paddingLeft);
-         horiRoot.getChildren().addAll(username, regisAcc, acceptName);
-         horiRoot.setAlignment(Pos.CENTER);
-        //  horiRoot.setTranslateX(60);
-        //  horiRoot.setTranslateY(25);
- 
-         VBox fillBox = new VBox();
-         fillBox.getChildren().addAll(horiRoot, horiRootage, horiRootPass, horiRootAccount, horiRootButton);
-         fillBox.setAlignment(Pos.TOP_CENTER);
-         fillBox.setPrefHeight(260);
-         fillBox.setMaxWidth(320);
-         fillBox.setSpacing(20);
-        //  fillBox.setStyle("-fx-background-color: #B9E5E8");
- 
+  
+        HBox horiRootage = new HBox(paddingLeft);
+        horiRootage.getChildren().addAll(age, regisAge, acceptAge);
+        horiRootage.setAlignment(Pos.CENTER);
+  
+        HBox horiRootPass = new HBox(paddingLeft);
+        horiRootPass.getChildren().addAll(pass, regisPass, acceptPass);
+        horiRootPass.setAlignment(Pos.CENTER);
+  
+        HBox horiRoot = new HBox(paddingLeft);
+        horiRoot.getChildren().addAll(username, regisAcc, acceptName);
+        horiRoot.setAlignment(Pos.CENTER);
+  
+        VBox fillBox = new VBox();
+        fillBox.getChildren().addAll(horiRoot, horiRootage, horiRootPass, horiRootAccount, ageVerify, horiRootButton);
+        fillBox.setAlignment(Pos.TOP_CENTER);
+        fillBox.setPrefHeight(260);
+        fillBox.setMaxWidth(320);
+        fillBox.setSpacing(20);
+  
         int paddingTop = 30;
-         VBox root = new VBox(paddingTop);
-         root.getChildren().addAll(titleLabel, fillBox);
-         root.setAlignment(Pos.TOP_CENTER);
-        //  root.setStyle("-fx-background-color: #7AB2D3");
-
-         Scene regis = new Scene(root, ScreenWidth, ScreenHeight);
-
-         return regis;
+        VBox root = new VBox(paddingTop);
+        root.getChildren().addAll(titleLabel, fillBox);
+        root.setAlignment(Pos.TOP_CENTER);
  
+        this.scenes.put("register", new Scene(root, ScreenWidth, ScreenHeight));
+    }
+
+    void createMenuScreen(){
+        Label titleLabel = this.showTitleTemplate();
+
+        int paddingTop = 30;
+        VBox root = new VBox(paddingTop);
+        root.getChildren().addAll(titleLabel);
+        root.setAlignment(Pos.TOP_CENTER);
+
+        this.scenes.put("menu", new Scene(root, ScreenWidth, ScreenHeight));
+    }
+    
+    // accesor for all scenes
+    public Scene getRegisScene(){
+        return this.scenes.get("register");
+    }
+
+    public Scene getMainScreen(){
+        return this.scenes.get("menu");
     }
 }
